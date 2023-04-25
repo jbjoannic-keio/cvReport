11b93a25b9ec9ac47c2f70c9328a795b1c1a17fa
jbjoannic-keio <jean-baptiste.joannic@keio.jp>
2023-04-25 17:50:41 +0900

    rep 2 works

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 - The cpp program can be found [here](exec/rep1.cpp)
 - It compute the intrinsic parameters of the camera given a set of 2 images minimum using opencv 3.0 library function `cv::cameraCalibration()`
 - `std::string path` and  `std::string resultPath` must be changed ([link to variable declaration](exec/rep1.cpp#L20))
+## Report 2 - Perspective Transformation - 24/04/2023
+- The cpp program can be found [here](exec/rep2.cpp)
+- `std::string imagePath` must be changed ([link to variable declaration](exec/rep2.cpp#L20))
---
+++ exec/rep2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "opencv2/opencv.hpp"
+
+void onMouse(int event, int x, int y, int flags, void *userdata)
+{
+    // Check if left mouse button is clicked and four points haven't been selected yet
+    if (event == cv::EVENT_LBUTTONDOWN && ((std::vector<cv::Point2f> *)userdata)->size() < 4)
+    {
+        // Save the point coordinates
+        ((std::vector<cv::Point2f> *)userdata)->push_back(cv::Point2f(x, y));
+        std::cout << "Selected point " << ((std::vector<cv::Point2f> *)userdata)->size() << ": (" << x << ", " << y << ")" << std::endl;
+    }
+}
+
+int main(int, char **)
+{
+    // Must be changed
+    std::string imagePath = "./images/1/default/1.jpg";
+    cv::Mat image = cv::imread(imagePath);
+    cv::namedWindow("Source", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Source", 800, 600);
+    cv::namedWindow("Warped", cv::WINDOW_NORMAL);
+    cv::resizeWindow("Warped", 600, 600);
+    if (image.empty())
+    {
+        std::cerr << "Error: could not read image " << imagePath << std::endl;
+        return -1;
+    }
+
+    std::vector<cv::Point2f> points;
+    std::vector<cv::Point2f> points2;
+    points2 = {{0, 0}, {300, 0}, {300, 300}, {0, 300}};
+
+    cv::setMouseCallback("Source", onMouse, &points);
+
+    while (points.size() < 4)
+    {
+        if (points.size() > 0)
+        {
+            cv::circle(image, points[points.size() - 1], 15, cv::Scalar(0, 255, 0), -1);
+        }
+        cv::imshow("Source", image);
+        cv::waitKey(50);
+    }
+
+    cv::Mat M = cv::getPerspectiveTransform(points, points2);
+    cv::Mat warped;
+    cv::warpPerspective(image, warped, M, cv::Size(300, 300));
+    cv::imshow("Warped", warped);
+    cv::waitKey(100000);
+}
